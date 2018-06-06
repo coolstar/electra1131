9a26d8b88a8acb533658cfa8776d7c32271c46d4
CoolStar <coolstarorganization@gmail.com>
2018-06-06 13:18:54 -0700

    Initial commit. multi_path exploit, uid0, kexecute and patchfinder64

---
+++ multi_path/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  multi_path
+//
+//  Created by Ian Beer on 5/28/18.
+//  Copyright © 2018 Ian Beer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ multi_path/exploit/sploit.h
@@ -0,0 +1,6 @@
+#ifndef multi_path_sploit_h
+#define multi_path_sploit_h
+
+kern_return_t multi_path_go(void);
+
+#endif
