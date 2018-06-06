7b1d386fd3e6f8582ed609a721911ef48b7b5a03
CoolStar <coolstarorganization@gmail.com>
2018-06-06 15:50:30 -0700

    Add posix utils

---
+++ multi_path/post-exploit/utilities/utils.h
@@ -0,0 +1,17 @@
+//
+//  utils.h
+//  electra
+//
+//  Created by Jamie on 27/01/2018.
+//  Copyright © 2018 Electra Team. All rights reserved.
+//
+
+#ifndef utils_h
+#define utils_h
+
+#include <stdio.h>
+
+int run(const char *cmd);
+char* itoa(long n);
+
+#endif /* utils_h */
