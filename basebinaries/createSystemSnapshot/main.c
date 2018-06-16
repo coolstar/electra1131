7f12c036a8270447fa5e6cf7bd7c666c5143f290
Pwn20wnd <pwn20wndstuff@gmail.com>
2018-06-16 21:23:26 +0300

    createSystemSnapshot improvements

--- basebinaries/createSystemSnapshot/main.c
+++ basebinaries/createSystemSnapshot/main.c
@@ -6,36 +6,59 @@
 // Copyright (c) 2018 Pwn20wnd
 // Copyright (c) 2018 ur0
 
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-#include <unistd.h>
 #include <sys/snapshot.h>
-#include <errno.h>
+#include <unistd.h>
 
-int do_create(int dirfd, const char *snap) {
-    int ret = fs_snapshot_create(dirfd, snap, 0);
+int find_rootfd(void) {
+    int dirfd = open("/", O_RDONLY, 0);
+    if (dirfd < 0) {
+        perror("open");
+        exit(1);
+    }
+    return dirfd;
+}
+
+int do_create(const char *snap) {
+    int rootfd = find_rootfd();
+    
+    int ret = fs_snapshot_create(rootfd, snap, 0);
     if (ret != 0)
         perror("fs_snapshot_create");
-    return ret;
+    return (ret);
 }
 
-int do_rename(int dirfd, const char *snap, const char *nw) {
+int do_delete(const char *snap) {
+    int rootfd = find_rootfd();
     
-    int ret = fs_snapshot_rename(dirfd, snap, nw, 0);
+    int ret = fs_snapshot_delete(rootfd, snap, 0);
+    if (ret != 0)
+        perror("fs_snapshot_delete");
+    return (ret);
+}
+
+int do_rename(const char *snap, const char *nw) {
+    int rootfd = find_rootfd();
+    
+    int ret = fs_snapshot_rename(rootfd, snap, nw, 0);
     if (ret != 0)
         perror("fs_snapshot_rename");
-    return ret;
+    return (ret);
 }
 
-int check_snapshot(int dirfd, const char *snap) {
+int check_snapshot(const char *snap) {
+    int rootfd = find_rootfd();
+    
     struct attrlist alist = {0};
     char abuf[2048];
     
     alist.commonattr = ATTR_BULK_REQUIRED;
     
-    int count = fs_snapshot_list(dirfd, &alist, &abuf[0], sizeof(abuf), 0);
+    int count = fs_snapshot_list(rootfd, &alist, &abuf[0], sizeof(abuf), 0);
     if (count < 0) {
         perror("fs_snapshot_list");
         return -1;
@@ -65,13 +88,15 @@ int check_snapshot(int dirfd, const char *snap) {
     return 0;
 }
 
-char *find_system_snapshot(int dirfd) {
+char *find_system_snapshot(void) {
+    int rootfd = find_rootfd();
+    
     struct attrlist alist = {0};
     char abuf[2048];
     
     alist.commonattr = ATTR_BULK_REQUIRED;
     
-    int count = fs_snapshot_list(dirfd, &alist, &abuf[0], sizeof(abuf), 0);
+    int count = fs_snapshot_list(rootfd, &alist, &abuf[0], sizeof(abuf), 0);
     if (count < 0) {
         perror("fs_snapshot_list");
         return NULL;
@@ -100,45 +125,74 @@ char *find_system_snapshot(int dirfd) {
     return NULL;
 }
 
-int main(int argc, char **argv) {
-    int dirfd = open("/", O_RDONLY, 0);
-    if (dirfd < 0) {
-        perror("open");
-        return -1;
+void clean_tmp_snapshots(void) {
+    int rootfd = find_rootfd();
+    
+    struct attrlist alist = {0};
+    char abuf[2048];
+    
+    alist.commonattr = ATTR_BULK_REQUIRED;
+    
+    int count = fs_snapshot_list(rootfd, &alist, &abuf[0], sizeof(abuf), 0);
+    if (count < 0) {
+        perror("fs_snapshot_list");
+        return;
     }
     
+    char *p = &abuf[0];
+    for (int i = 0; i < count; i++) {
+        char *field = p;
+        uint32_t len = *(uint32_t *)field;
+        field += sizeof(uint32_t);
+        attribute_set_t attrs = *(attribute_set_t *)field;
+        field += sizeof(attribute_set_t);
+        
+        if (attrs.commonattr & ATTR_CMN_NAME) {
+            attrreference_t ar = *(attrreference_t *)field;
+            const char *name = field + ar.attr_dataoffset;
+            field += sizeof(attrreference_t);
+            
+            if (strstr(name, "electra-tmp-snapshot-")) {
+                do_delete(name);
+            }
+        }
+        
+        p += len;
+    }
+}
+
+int main(int argc, char **argv) {
     int ret = -1;
     
-    char *system_snapshot = find_system_snapshot(dirfd);
-    
-    printf("%s\n", system_snapshot);
+    char *system_snapshot = find_system_snapshot();
     
     const char *nw = strdup("electra-system-snapshot");
-    ret = do_rename(dirfd, system_snapshot, nw);
-    if (ret) {
-        if (errno == 17) {
-            char name[256];
-            sprintf(name, "electra-tmp-snapshot-%lu", time(NULL));
-            char *p = &name[0];
-            nw = strdup(p);
-            printf("%s\n", nw);
-            ret = do_rename(dirfd, system_snapshot, nw);
-            if (ret) {
-                return -2;
-            }
-        } else {
+    
+    if (check_snapshot(nw) == 1) {
+        char name[256];
+        sprintf(name, "electra-tmp-snapshot-%lu", time(NULL));
+        char *p = &name[0];
+        nw = strdup(p);
+        ret = do_rename(system_snapshot, nw);
+        if (ret) {
+            return -2;
+        }
+    } else {
+        ret = do_rename(system_snapshot, nw);
+        if (ret) {
             return -3;
         }
     }
     
-    ret = do_create(dirfd, system_snapshot);
+    ret = do_create(system_snapshot);
     
     if (ret) {
         return -4;
     }
     
+    clean_tmp_snapshots();
+    
     free(system_snapshot);
-    close(dirfd);
     
     return 0;
 }
